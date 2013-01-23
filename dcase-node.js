b1cede9761fcffe24b8200e46a3854f72023d6b7
Joseph Lee <leejonggun0129@gmail.com>
2013-01-23 14:31:28 +0900

    TODO application layer

--- dcase-node.js
+++ dcase-node.js
@@ -86,145 +86,122 @@ function createSampleNode() {
 	var strategy_children = [
 		{ name: "SubGoal 1", type: "Goal", desc: "Link layerは正常である",
 		children: [
-					{ name: "Context 1.1", type: "Context", desc: "@接続方法:イーサネット、無線LAN" },
-					{ name: "Strategy", type: "Strategy", desc: "接続方式により議論" ,
+			{ name: "Context 1.1", type: "Context", desc: "@接続方法:イーサネット、無線LAN" },
+			{ name: "Strategy", type: "Strategy", desc: "接続方式により議論" ,
+			children: [
+				{ name: "SubGoal 1.1(or)", type: "Goal",  desc: "イーサネットで接続している" ,
+				children: [
+					{ name: "Strategy", type: "Strategy", desc: "PCや周辺機器の状態により判断" ,
 					children: [
-								{ name: "SubGoal 1.1(or)", type: "Goal",  desc: "イーサネットで接続している" ,
-								children: [
-											{ name: "Strategy", type: "Strategy", desc: "PCや周辺機器の状態により判断" ,
-											children: [
-														{ name: "SubGoal 1.1.1", type: "Goal",  desc: "PCに繋がっているネットワークケーブルが途切れていない" },
-														{ name: "SubGoal 1.1.2", type: "Goal",  desc: "ハブが壊れていない" },
-														{ name: "SubGoal 1.1.3", type: "Goal",  desc: "イーサネットカードが認識されている" ,
-														children: [
-																	{ name: "Evidence", type: "Evidence", desc: "CheckNIC.ds" }
-																			]
-														},
-														{ name: "SubGoal 1.1.4", type: "Goal",  desc: "PCでイーサネットインターフェースが有効になっている" ,
-														children: [
-																	{ name: "Evidence", type: "Evidence", desc: "Connection.ds" }
-																			]
-														}
-																]
-											}
-													]
-								},
-								{ name: "SubGoal 1.2(or)", type: "Goal",  desc: "無線LANで接続している" ,
-								children: [
-											{ name: "Strategy", type: "Strategy", desc: "PCや周辺機器の状態により判断" ,
-											children: [
-														{ name: "SubGoal 1.2.1", type: "Goal",  desc: "電波信号が十分に強い" },
-														{ name: "SubGoal 1.2.2", type: "Goal",  desc: "電波干渉がない" },
-														{ name: "SubGoal 1.2.3", type: "Goal",  desc: "無線LANカードが認識されている" ,
-														children: [
-																	{ name: "Evidence", type: "Evidence", desc: "CheckNIC.ds" }
-																			]
-														},
-														{ name: "SubGoal 2.2", type: "Goal",  desc: "PCで無線LANインターフェースが有効になっている" ,
-														children: [
-																	{ name: "Evidence 2.1", type: "Evidence", desc: "Connection.ds" }
-																			]
-														}
-																]
-											}
-													]
-								}
+						{ name: "SubGoal 1.1.1", type: "Goal",  desc: "PCに繋がっているネットワークケーブルが途切れていない" },
+						{ name: "SubGoal 1.1.2", type: "Goal",  desc: "ハブが壊れていない" },
+						{ name: "SubGoal 1.1.3", type: "Goal",  desc: "イーサネットカードが認識されている" ,
+						children: [
+							{ name: "Evidence", type: "Evidence", desc: "CheckNIC.ds" }
+											]
+						},
+						{ name: "SubGoal 1.1.4", type: "Goal",  desc: "PCでイーサネットインターフェースが有効になっている" ,
+						children: [
+							{ name: "Evidence", type: "Evidence", desc: "Connection.ds" }
+											]
+						}
 										]
 					}
+									]
+				},
+
+				{ name: "SubGoal 1.2(or)", type: "Goal",  desc: "無線LANで接続している" ,
+				children: [
+					{ name: "Strategy", type: "Strategy", desc: "PCや周辺機器の状態により判断" ,
+					children: [
+						{ name: "SubGoal 1.2.1", type: "Goal",  desc: "電波信号が十分に強い" },
+						{ name: "SubGoal 1.2.2", type: "Goal",  desc: "電波干渉がない" },
+						{ name: "SubGoal 1.2.3", type: "Goal",  desc: "無線LANカードが認識されている" ,
+						children: [
+							{ name: "Evidence", type: "Evidence", desc: "CheckNIC.ds" }
+											]
+							},
+							{ name: "SubGoal 2.2", type: "Goal",  desc: "PCで無線LANインターフェースが有効になっている" ,
+							children: [
+								{ name: "Evidence 2.1", type: "Evidence", desc: "Connection.ds" }
+												]
+							}
+										]
+					}
+									]
+				}
+								]
+			}
 							]
 		},
-
-//		{ name: "SubGoal 2", type: "Goal", desc: "Data Link layerは正常である",
-//		children: [
-//					{ name: "Context 2.1", type: "Context",  desc: "@IP Address:192.168.59.101" },
-//					{ name: "Strategy", type: "Strategy",  desc: "接続方式により議論" ,
-//					children: [
-//								{ name: "Context 2.1", type: "Context",  desc: "@接続方法:イーサネット、無線LAN" },
-//								{ name: "SubGoal 2.1", type: "Goal",  desc: "PCでイーサネットインターフェースが有効になっている" ,
-//								children: [
-//											{ name: "Evidence 2.1", type: "Evidence", desc: "Connection.ds" }
-//													]
-//								},
-//								{ name: "SubGoal 2.2", type: "Goal",  desc: "PCで無線LANインターフェースが有効になっている" ,
-//								children: [
-//											{ name: "Evidence 2.1", type: "Evidence", desc: "Connection.ds" }
-//													]
-//								}
-//										]
-//					}
-//							]
-//		},
 
 			//パケットを送信元から宛先まで届ける全工程を担っている。
 			//仮想的なパケット交換ネットワークを構築、ホストとホスト間の通信を実現。ICMP(Internet Control Message Protocol)等もここ。つまり、pingによるチェックはここまで。
 			//同じネットワーク媒体上に接続されているコンピュータ間同士だけではなく、異なるネットワーク媒体上に接続されているコンピュータの間でも通信を行えるようにする。(IP)アドレス付け。(ゲートウェイ内外の)ルーティングプロトコル。
 		{ name: "SubGoal 2", type: "Goal", desc: "Internet layerは正常である",
 		children: [
-						{ name: "Context 1.1", type: "Context", desc: "@IP Address:192.168.59.101" },
-						{ name: "Strategy", type: "Strategy", desc: "Internet layerの持つ役割を基に議論" ,
+			{ name: "Context 1.1", type: "Context", desc: "@IP Address:192.168.59.101" },
+			{ name: "Strategy", type: "Strategy", desc: "Internet layerの持つ役割を基に議論" ,
+			children: [
+//				{ name: "SubGoal 2.1", type: "Goal", desc: "IP Addressが割り当てられている" ,//IP AddressがLAN外かLAN内か?
+//				children: [
+//					{ name: "Evidence 2.1", type: "Evidence", desc: "CheckIPAddress.ds" }
+//									]
+//				},
+
+				{ name: "SubGoal 2.2", type: "Goal", desc: "ルーティングができる" ,
+				children: [
+					{ name: "Strategy", type: "Strategy", desc: "ルーティングのされ方で議論" ,
+					children: [
+						{ name: "SubGoal 2.2.1", type: "Goal", desc: "直接ルーティングできる" ,
 						children: [
-									{ name: "SubGoal 2.1", type: "Goal", desc: "IP Addressが割り当てられている" ,//IP AddressがLAN外かLAN内か?
-									children: [
-												{ name: "Evidence 2.1", type: "Evidence", desc: "CheckIPAddress.ds" }
-														]
-									},
-									{ name: "SubGoal 2.2", type: "Goal", desc: "ルーティングができる" ,
-									children: [
-												{ name: "Strategy", type: "Strategy", desc: "ルーティングのされ方で議論" ,
-												children: [
-															{ name: "SubGoal 2.2.1", type: "Goal", desc: "直接ルーティングできる" ,
-															children: [
-																		{ name: "Evidence 2.2.1", type: "Evidence", desc: "RoutingDirectly.ds" }
-																				]
-															},
-															{ name: "SubGoal 2.2.2", type: "Goal", desc: "ゲートウェイを通してルーティングできる" ,
-															children: [
-																		{ name: "Evidence 2.2.1", type: "Evidence", desc: "RoutingDefault.ds" }
-																				]
-															}
-																	]
-												}
-														]
-									},
-									{ name: "SubGoal 2.3", type: "Goal", desc: "firewall設定によりIP Addressレベルでパケット情報が破棄されない" ,
-									children: [
-												{ name: "Strategy", type: "Strategy", desc: "INPUT, FORWARD, OUTPUT別に確認する" ,
-												children: [
-															{ name: "SubGoal 2.3.1", type: "Goal", desc: "INPUTチェーンではパケットを受け入れている" ,
-															children: [
-																		{ name: "Evidence", type: "Evidence", desc: "FirewallIPInput.ds" }
-																				]
-															},
-															{ name: "SubGoal 2.3.2", type: "Goal", desc: "FORWARDチェーンではパケットを受け入れている" ,
-															children: [
-																		{ name: "Evidence", type: "Evidence", desc: "FirewallIPForward.ds" }
-																				]
-															},
-															{ name: "SubGoal 2.3.3", type: "Goal", desc: "OUTPUTチェーンではパケットを受け入れている" ,
-															children: [
-																		{ name: "Evidence", type: "Evidence", desc: "FirewallIPOutput.ds" }
-																				]
-															}
-																	]
-												
-												}
-														]
-									},
-									{ name: "SubGoal 2.4", type: "Goal", desc: "pingが通る" ,
-										children: [
-													{ name: "Context 1.1", type: "Context", desc: "@IP Address:192.168.59.101" },
-													{ name: "Evidence", type: "Evidence", desc: "Ping.ds" }
-															]
-									
-									}
+							{ name: "Evidence 2.2.1", type: "Evidence", desc: "RoutingDirectly.ds" }
 											]
-						}
-//					{ name: "Evidence 2.1", type: "Evidence", desc: "CheckIPAddress.ds" },
-//					{ name: "Evidence 2.5", type: "Evidence", desc: "FirewallIPOutput.ds" },//IP Level
-//					{ name: "Evidence 2.6", type: "Evidence", desc: "FirewallIPInput.ds" }//IP Level
-//					{ name: "Evidence 2.2", type: "Evidence", desc: "RoutingDirectly.ds" },
-//					{ name: "Evidence 2.3", type: "Evidence", desc: "RoutingDirectly.ds" },
-//					{ name: "Evidence 2.4", type: "Evidence", desc: "Ping.ds" },
+						},
+						{ name: "SubGoal 2.2.2", type: "Goal", desc: "ゲートウェイを通してルーティングできる" ,
+						children: [
+							{ name: "Evidence 2.2.1", type: "Evidence", desc: "RoutingDefault.ds" }
+											]
+							},
+						{ name: "SubGoal 2.2.3", type: "Goal", desc: "経路の各ルータが(ルーティングできる)パケットを破棄しない" }
+										]
+					}
+									]
+				},
+
+				{ name: "SubGoal 2.3", type: "Goal", desc: "firewall設定によりIP Addressレベルでパケット情報が破棄されない" ,
+				children: [
+					{ name: "Strategy", type: "Strategy", desc: "INPUT, FORWARD, OUTPUT別に確認する" ,
+					children: [
+						{ name: "SubGoal 2.3.1", type: "Goal", desc: "INPUTチェーンではパケットを受け入れている" ,
+						children: [
+							{ name: "Evidence", type: "Evidence", desc: "FirewallIPInput.ds" }
+											]
+							},
+							{ name: "SubGoal 2.3.2", type: "Goal", desc: "FORWARDチェーンではパケットを受け入れている" ,
+							children: [
+								{ name: "Evidence", type: "Evidence", desc: "FirewallIPForward.ds" }
+												]
+								},
+								{ name: "SubGoal 2.3.3", type: "Goal", desc: "OUTPUTチェーンではパケットを受け入れている" ,
+								children: [
+									{ name: "Evidence", type: "Evidence", desc: "FirewallIPOutput.ds" }
+													]
+									}
+										]
+					}
+									]
+				},
+
+				{ name: "SubGoal 2.4", type: "Goal", desc: "pingが通る" ,
+				children: [
+					{ name: "Context 2.1", type: "Context", desc: "@IP Address:192.168.59.101<br>@OS:ubuntu12.04LTS 64bit" },
+					{ name: "Context 2.2", type: "Context", desc: "@Destination:192.168.59.102" },
+					{ name: "Evidence", type: "Evidence", desc: "Ping.ds" }
+									]
+				}
+								]
+			}
 							]
 		},
 
@@ -232,25 +209,77 @@ function createSampleNode() {
 //任意のサイズのデータを送るために、データの分割と再構築を行う。アプリケーションにパケットを渡すときにPort番号で識別している
 		{ name: "SubGoal 4", type: "Goal", desc: "Transport layerは正常である",
 			children: [
-						{ name: "Evidence 3.1", type: "Evidence", desc: "FirewallPortOutput.ds" },// TCP/UDP Port
-						{ name: "Evidence 3.2", type: "Evidence", desc: "FirewallPortInput.ds" }// TCP/UDP Port
-					  ]
+				{ name: "Context 4.1", type: "Context", desc: "@プロトコル:TCP, UDP" },
+				{ name: "Strategy", type: "Strategy", desc: "プロトコルにより判断する" ,
+				children: [
+					{ name: "SubGoal 4.1", type: "Goal", desc: "TCPについて議論" ,// TCP/UDP Port,再送制御、順序制御、フロー制御、輻輳制御
+					children: [
+						{ name: "Strategy", type: "Strategy", desc: "firewall設定を考慮する" ,
+						children: [
+							{ name: "SubGoal 4.1.1", type: "Goal", desc: "受信するTCPプロトコルのパケットを破棄しない" ,
+							children: [
+								{ name: "Evidence 4.1.1", type: "Evidence", desc: "FirewallTCPInput.ds" }// TCP/UDP
+												]
+							},
+							{ name: "SubGoal 4.1.2", type: "Goal", desc: "中継するTCPプロトコルのパケットを破棄しない" ,
+							children: [
+								{ name: "Evidence 4.1.2", type: "Evidence", desc: "FirewallTCPForward.ds" }// TCP/UDP
+												]
+							},
+							{ name: "SubGoal 4.1.3", type: "Goal", desc: "送信するTCPプロトコルのパケットを破棄しない" ,
+							children: [
+								{ name: "Evidence 4.1.3", type: "Evidence", desc: "FirewallTCPOutput.ds" }// TCP/UDP
+												]
+							},
+							{ name: "SubGoal 4.1.4", type: "Goal", desc: "その他TCPプロトコルに関しての設定を確認する" }//ポート指定、IP Address指定、オプション等
+											]
+						}
+										]
+					},
+
+					{ name: "SubGoal 4.2", type: "Goal", desc: "UDPについて議論" ,// TCP/UDP Port
+					children: [
+						{ name: "Strategy", type: "Strategy", desc: "firewall設定を考慮する" ,
+						children: [
+							{ name: "SubGoal", type: "Goal", desc: "受信するUDPプロトコルのパケットを破棄しない" ,
+							children: [
+						{ name: "Evidence 4.2.1", type: "Evidence", desc: "FirewallUDPOutput.ds" },// TCP/UDP Port
+												]
+							},
+							{ name: "SubGoal", type: "Goal", desc: "中継するUDPプロトコルのパケットを破棄しない" ,
+							children: [
+						{ name: "Evidence 4.2.2", type: "Evidence", desc: "FirewallUDPForward.ds" },// TCP/UDP Port
+												]
+							},
+							{ name: "SubGoal", type: "Goal", desc: "送信するUDPプロトコルのパケットを破棄しない" ,
+							children: [
+						{ name: "Evidence 4.2.3", type: "Evidence", desc: "FirewallUDPInput.ds" }// TCP/UDP Port
+												]
+							},
+							{ name: "SubGoal 4.2.4", type: "Goal", desc: "その他UDPプロトコルに関しての設定を確認する" }
+											]
+						}
+										]
+					}
+									]
+				}
+								]
 		},
+
+//Presentation layer->ネットワークに流れるデータの意味を統一。コードの変換以外にデータの暗号化や、データの圧縮なども
 		{ name: "SubGoal 5", type: "Goal", desc: "Application layerは正常である",
+		children: [
+			{ name: "Strategy", type: "Strategy", desc: "(アプリケーション層で議論する)" ,
 			children: [
-						{ name: "Evidence 2.1", type: "Evidence", desc: "Nslookup.ds" }
-					  ]
+				{ name: "SubGoal 5.1", type: "Goal", desc: "名前解決できる" ,
+				children: [
+					{ name: "Evidence 5.1", type: "Evidence", desc: "Nslookup.ds" }
+									]
+				}
+								]
+			}
+							]
 		}
-//		{ name: "SubGoal 6", type: "Goal", desc: "Presentation layerは正常である",
-//			children: [
-//						{ name: "Evidence2.1", type: "Evidence", desc: "FirewallInput.ds" }
-//					  ]
-//		},
-//		{ name: "SubGoal 7", type: "Goal", desc: "Application layerは正常である",
-//			children: [
-//						{ name: "Evidence 2.1", type: "Evidence", desc: "Nslookup.ds" }
-//					  ]
-//		}
 	];
 	return createNodeFromJson2({
 		name: "TopGoal", type: "Goal",
